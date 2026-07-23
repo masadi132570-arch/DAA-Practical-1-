7ff830e4b4ea1cd299e117ed2cb40e33d1636cbf
Vyshnavi masadi <masadi.132570@marwadiuniversity.ac.in>
2026-07-23 17:41:56 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <utility> 
+using namespace std;
+void bubbleSort(int arr[], int n) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true; 
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int main() {
+    int data[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(data) / sizeof(data[0]);
+    cout << "Original array: \n";
+    printArray(data, size);
+    bubbleSort(data, size);
+    cout << "Sorted array in ascending order: \n";
+    printArray(data, size)
+    return 0;
+}
